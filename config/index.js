4a0d47ce7a5be33df5a0d1c52aa4896820826d60
Chris Troutner <chris.troutner@gmail.com>
2023-06-05 15:44:42 -0700

    Updating latest price target

--- config/index.js
+++ config/index.js
@@ -6,9 +6,9 @@ const config = {
   // These target prices are the last price that the loan was rebalanced at. If
   // the price fluctuates more than 10% away from these targets, then the system
   // will send an email.
-  btcTarget: 28278,
+  btcTarget: 25663,
   ethTarget: 1920,
-  avaxTarget: 15.34
+  avaxTarget: 13.88
 }
 
 module.exports = config

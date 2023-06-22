c2db8174c55fff64822cffaeedb7779aa261662e
Chris Troutner <chris.troutner@gmail.com>
2023-06-22 07:22:05 -0700

    Updating target price for BTC

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ const config = {
   // These target prices are the last price that the loan was rebalanced at. If
   // the price fluctuates more than 10% away from these targets, then the system
   // will send an email.
-  btcTarget: 25663,
+  btcTarget: 30172,
   ethTarget: 1741,
   avaxTarget: 11.54
 }

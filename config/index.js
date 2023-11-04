39c2bccbfaeac30c0fc13e6ee1bc84a317eb87ad
Chris Troutner <chris.troutner@gmail.com>
2023-11-04 09:23:54 -0700

    Updating target for ETH

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ const config = {
   // the price fluctuates more than 10% away from these targets, then the system
   // will send an email.
   btcTarget: 33791,
-  ethTarget: 1666,
+  ethTarget: 1831,
   avaxTarget: 11.98
 }
 
